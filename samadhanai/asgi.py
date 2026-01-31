ec8292602c67d64404275e1a87ef68ceac4778fd
Alokxt <manawatalok@gmail.com>
2026-01-31 22:20:32 +0530

    minor configurations

--- samadhanai/asgi.py
+++ samadhanai/asgi.py
@@ -1,30 +1,20 @@
-"""
-ASGI config for samadhanai project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from chatapp import routing 
-from . import settings
-from chatapp.middleware import JWTAuthMiddleWare
-
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samadhanai.settings')
 
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chatapp.middleware import JWTAuthMiddleWare
+from chatapp.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleWare(AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        ))
+    "http": django_asgi_app,
+    "websocket": JWTAuthMiddleWare(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
 })
